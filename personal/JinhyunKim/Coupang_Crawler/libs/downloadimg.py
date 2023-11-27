c23c64e61f91602c63b332f6da07efe1fa360c13
jh-isnotavailable <jhk1618@korea.ac.kr>
2023-11-28 02:35:52 +0900

    Coupang Crawler-주석처리 및 코드 수정 완료

--- personal/JinhyunKim/Coupang_Crawler/libs/downloadimg.py
+++ personal/JinhyunKim/Coupang_Crawler/libs/downloadimg.py
@@ -4,48 +4,50 @@ import os
 from urllib.request import urlretrieve, urlopen
 import re
 
-
 def clean_filename(title):
+    # 파일 이름에서 특수 문자와 공백을 제거하는 함수
     cleaned_title = re.sub(r'[\/:*?"<>|\s]', '', title)
     return cleaned_title
 
-
 def extract_jpeg_urls(soup):
-    # Convert the soup object to a string
+    # BeautifulSoup 객체를 문자열로 변환
     html_content = str(soup)
 
-    # Use regular expression to find all occurrences of "origin":"//(image URL).jpeg"
-    # and capture only the //(image URL).jpeg part
+    # 정규 표현식을 사용하여 "origin":"//(image URL)" 패턴에 해당하는 모든 URL 추출
+    # //(image URL) 부분만 캡처
     matches = re.findall(r'"origin":"(//[^"]+)"', html_content)
 
-    # Return the list of extracted .jpeg URLs
+    # 추출된 URL 목록 반환
     return matches
 
-
 def downloadimg(df):
+    # DataFrame의 각 행에 대해 이미지 다운로드 수행
     for index, row in df.iterrows():
         link = row['Link']
-        # df['Name'] 값을 clean_filename 함수를 통해 정제
+        # df['Name'] 값 정제
         name = clean_filename(row['Name'])
 
-        text = (getres(link))
+        text = getres(link)
         soup = BeautifulSoup(text, 'html5lib')
-        html_content = str(soup)
-        jpeg_urls = extract_jpeg_urls(html_content)
+        jpeg_urls = extract_jpeg_urls(soup)
 
-
-        fdir = f'.//data//sweatshirt//{name}'
+        # 이미지를 저장할 디렉토리 생성
+        fdir = f'.//data//cat//toilet//{name}'
         if not os.path.exists(fdir):
             os.makedirs(fdir)
 
+        # 각 이미지 URL에 대해 다운로드 시도
         for urls in jpeg_urls:
             img_url = f'https:{urls}'
             try:
+                # URL에서 파일명 추출 및 저장 경로 설정
                 filename = urls.split('/')[-1]
                 file_path = os.path.join(fdir, filename)
 
+                # 이미지 다운로드 및 저장
                 urlretrieve(img_url, file_path)
                 print(f"Downloaded {filename}")
 
             except Exception as e:
+                # 다운로드 오류 처리
                 print(f"Error downloading {filename}: {e}")
