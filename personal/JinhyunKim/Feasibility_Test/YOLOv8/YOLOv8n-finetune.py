8e93c9495a5e3aaf7cce05de18252cdc3a8eb3ea
jh-isnotavailable <jhk1618@korea.ac.kr>
2023-12-16 02:49:18 +0900

    /JinhyunKim/Feasibility_Test/YOLOv8 주석 처리 완료

--- personal/JinhyunKim/Feasibility_Test/YOLOv8/YOLOv8n-finetune.py
+++ personal/JinhyunKim/Feasibility_Test/YOLOv8/YOLOv8n-finetune.py
@@ -3,31 +3,32 @@ import torch
 from PIL import Image
 
 
+# yolo v8 nano 모델 파인튜닝 함수
 def train_yolo():
-    yolo = YOLO('yolov8n.pt')
-    yolo.train(data='C://Users//jhk16//PycharmProjects//Wise_Ad-//personal//JinhyunKim//Fiftyone//merged_final_new//dataset.yaml', epochs=10, batch=16,save=True)
+    yolo = YOLO('yolov8n.pt')   # pretrained 된 모델 지정
+    yolo.train(data='D://Data//merged_final_new//dataset.yaml', epochs=10, batch=16,save=True)   # Custom 데이터셋의 yaml 파일 지정
 
+# 중단된 시점부터 학습 재개하는 함수
 def resume_yolo():
-    model = YOLO("C://Users//jhk16//PycharmProjects//Wise_Ad-//runs//detect//train9//weights//last.pt")
-    results = model.train(data='D:\Data\merged_final\dataset.yaml', epochs=2, batch=32, resume=True,save=True)
+    model = YOLO(".//runs//detect//train9//weights//last.pt") # 저장된 pretrained model 디렉토리 지정
+    results = model.train(data='D://Data//merged_final_new//dataset.yaml', epochs=2, batch=32, resume=True,save=True)   # Custom 데이터셋의 yaml 파일 지정
 
+# 파인튜닝 된 모델로부터 Inference Test 진행하는 함수
 def test_yolo():
-    # Load the YOLO model
-    model = YOLO("C://Users//jhk16//PycharmProjects//Wise_Ad-//runs\detect//train3\weights//best.pt")
+    model = YOLO("C://Users//jhk16//PycharmProjects//Wise_Ad-//runs\detect//train3\weights//best.pt")   # 파인튜닝 된 모델 지정
+    results = model(".//Inference_Test//sweatshirt_test.jpg")
 
-    # Run the model on an image
-    results = model("C://Users//jhk16//PycharmProjects//Wise_Ad-//personal//JinhyunKim//Fiftyone//YOLOv8//sweatshirt_test.jpg")
-
-    # Show the results
+    # 사진 위에 Inference 결과 표시
     for r in results:
-        im_array = r.plot()  # plot a BGR numpy array of predictions
-        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-        im.show()  # show image
-        im.save('results.jpg')  # save image
+        im_array = r.plot()
+        im = Image.fromarray(im_array[..., ::-1])
+        im.show()
+        im.save('results.jpg')  # 결과 사진 저장
 
+# 파인튜닝 된 모델에 대한 성능 평가 metrics 추출
 def validate_yolo():
-    model = YOLO("C://Users//jhk16//PycharmProjects//Wise_Ad-//runs\detect//train3\weights//best.pt")
-    model.val(data="D://Data//Coupang_Sweatshirt_2000.v4i.yolov8//dataset.yaml",plots=True)
+    model = YOLO("C://Users//jhk16//PycharmProjects//Wise_Ad-//runs\detect//train3\weights//best.pt")   # 파인튜닝 된 모델 지정
+    model.val(data="D://Data//Coupang_Sweatshirt_2000.v4i.yolov8//dataset.yaml",plots=True) # 성능 평가에 사용할 데이터셋 yaml 파일 지정
 
 if __name__ == '__main__':
     test_yolo()
