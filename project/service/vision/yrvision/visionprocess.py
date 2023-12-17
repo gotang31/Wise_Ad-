0011439c68a8bc28e825ef17419132782775d23d
gotang31 <kotaewook31@gmail.com>
2023-12-18 00:10:23 +0900

    service

--- project/service/vision/yrvision/visionprocess.py
+++ project/service/vision/yrvision/visionprocess.py
@@ -1,8 +1,8 @@
-from .videopreprocess import download_videos, create_imgframes
+from .videopreprocess import clean_filename, download_videos, create_imgframes
 from .utils import inference, similarity_result, subject_extraction, key_extraction
 import os
 import pickle
-
+from pytube import YouTube
 
 def input_url(model_detect, model_sim, url, fdir):
     '''
@@ -15,7 +15,8 @@ def input_url(model_detect, model_sim, url, fdir):
         os.makedirs(fdir)
 
     video_title, video_time = download_videos(fdir, url)
-    image_dir = create_imgframes(fdir, video_title)
+    image_dir = create_imgframes(fdir, video_title) # image_dir = {fdir}/{video_title}
+    print('videoprocess')
     
     # Detection
     # 이미지 시간 순 리스트 : 이미지 저장할 때 0부터 시작해서 저장해야 함
@@ -26,27 +27,38 @@ def input_url(model_detect, model_sim, url, fdir):
         result_list = inference(model_detect, image_dir, img)
         inference_list.extend(result_list)
     
+    print('detection')
+    
     # key category extraction
     video_subject = subject_extraction(inference_list)
     key_result, split_time_list = key_extraction(inference_list, video_time)
 
+    print('key')
+
     # inference result 저장 -> 나중에 단일 이미지 input 될 때 이용하기 위해 ; 사실 이게 영상 추론 DB가 될 수도 있기에 pickle 형식이 아니라 다른 좋은 형식이 있다면 알려주세요.
     inference_result = {f'{image_dir}' : inference_list}
-    with open(f'./{url}.pickle' , 'wb') as fp:
+    with open(f'{image_dir}.pickle' , 'wb') as fp:
         pickle.dump(inference_result, fp, protocol = pickle.HIGHEST_PROTOCOL)
+    
+    print('inference result 저장')
     
     # similarity
     similar_itemid_list, category_list = similarity_result(model_sim, key_result, image_dir)
 
-    return similar_itemid_list, category_list, video_subject, split_time_list
+    print('similarity')
+
+    return similar_itemid_list, category_list, split_time_list, video_subject 
 
 
-def input_img(model_sim, url, image_name):
+def input_img(model_sim, url, fdir, image_name, video_subject):
     '''
     input_url에서 이미 inference process와 result 저장을 진행하기에, img_name(index)만 받으면 해당 추론 결과만 가져오면 된다.
     영상 분할에서 만들어지는 이미지 프레임의 파일 이름(0.jpg ~ (n-1).jpg)는 시간 정보와 file directory 정보를 담고 있기에 매우 중요한 파라미터이다.
     '''
-    with open(f'./{url}.pickle' , 'wb') as fp:
+    yt = YouTube(url)
+    video_title = clean_filename(yt.title)
+
+    with open(f'{fdir}/{video_title}.pickle' , 'rb') as fp:
         inference_result = pickle.load(fp)
 
     image_dir = inference_result.keys()
@@ -58,4 +70,4 @@ def input_img(model_sim, url, image_name):
     # similarity : 유사도 추론 결과는 저장할 수 없다. image만 받으면 detection 추론 결과를 가지고 유사도 추론은 실시간으로
     similar_itemid_list, category_list = similarity_result(model_sim, image_result, image_dir)
 
-    return similar_itemid_list, category_list
+    return similar_itemid_list, category_list, video_subject
