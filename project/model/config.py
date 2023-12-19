9aca5701fa1c4d9a8ab08de4bbbc562e952859b7
iam1004yang <46566770+iam1004yang@users.noreply.github.com>
2023-12-19 19:43:52 +0900

    RecSys 찐 Final

--- project/model/config.py
+++ project/model/config.py
@@ -6,14 +6,14 @@ import psycopg2 as pg
 
 def get_gds():
     # neo4j graph DB에 따라서 설정을 바꿔주시면 됩니다.
-    DB_URI = 'bolt://localhost:7687'
-    DB_USER = 'neo4j'
-    DB_PASSWORD = 'inisw_test'
+    #DB_URI = 'bolt://localhost:7687'
+    #DB_USER = 'neo4j'
+    #DB_PASSWORD = 'inisw_test'
     # eno4j sandbox (온라인 DB)를 사용할 경우 위에 것을 comment하고 다음을 uncomment 해주세요.
     # 현재 작성되어 있는 neo4j sandbox는 12월 21일 까지 유효합니다.
-    #DB_URI = 'bolt://18.212.191.71:7687'
-    #DB_USER = 'neo4j'
-    #DB_PASSWORD = 'burns-mints-shocks'	
+    DB_URI = 'bolt://3.228.16.212:7687'
+    DB_USER = 'neo4j'
+    DB_PASSWORD = 'spot-aggravations-wiggles'	
 
     # Python용 driver 생성
     gds = GraphDataScience(DB_URI, auth=(DB_USER, DB_PASSWORD))
